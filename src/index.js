e1f974c119045834b38e38ed22901a97e1eef7e2
Akshat Verma <Vermaakshat420@gmail.com>
2022-08-03 17:39:45 +0530

    third commit adding comments

--- src/index.js
+++ src/index.js
@@ -22,4 +22,16 @@ first we createContext() in context.js and then make a provider AppProvider func
 AppProvider needs to wrapped around all element to make it work normally so, we wrapped it in index.js 
 
 
+when we make createContext we wrap around all the components while sending value using provider
+like this in App.js
+const Detail = createContext();
+<Detail.provider value="ajda" || value = {object/array etc}>
+  <CompA />
+  <CompB />
+  ...
+</Detail.Provider>
+
+but this time we make diff file only for create context so for that syntax is in Context.js 
+but we still have to wrap it around so we do that in index.js or we can do it in App.js too
+
 */
